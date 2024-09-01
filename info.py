1e4ba46f4be943b5ae926e79aafb0eb15252e0eb
Gulabsingh008 <gulabsinghrajput008@gmail.com>
2024-09-02 05:14:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,7 +61,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'gyanilinks.com')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'gtlinks.me')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'c3ab7f68857a356db3bbddc7995fd0bd23aedeb2')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
